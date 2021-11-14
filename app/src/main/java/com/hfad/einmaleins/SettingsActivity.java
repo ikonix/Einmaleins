5203eeee620aa4d1f31cdc18d1839d1c2aef3469
Alexander <ikonix1304@gmail.com>
2021-11-14 18:14:22 +0100

    settings design

--- app/src/main/java/com/hfad/einmaleins/SettingsActivity.java
+++ app/src/main/java/com/hfad/einmaleins/SettingsActivity.java
@@ -6,9 +6,64 @@ import android.os.Bundle;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    private int mode = 0;
+    private int[] numbers = new int[10];
+    private boolean addition = false;
+    private boolean subtraction = false;
+    private boolean multiplication = true;
+    private boolean division = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+    }
+
+    public int getMode() {
+        return mode;
+    }
+
+    public void setMode(int mode) {
+        this.mode = mode;
+    }
+
+    public int[] getNumbers() {
+        return numbers;
+    }
+
+    public void setNumbers(int[] numbers) {
+        this.numbers = numbers;
+    }
+
+    public boolean isAddition() {
+        return addition;
+    }
+
+    public void setAddition(boolean addition) {
+        this.addition = addition;
+    }
+
+    public boolean isSubtraction() {
+        return subtraction;
+    }
+
+    public void setSubtraction(boolean subtraction) {
+        this.subtraction = subtraction;
+    }
+
+    public boolean isMultiplication() {
+        return multiplication;
+    }
+
+    public void setMultiplication(boolean multiplication) {
+        this.multiplication = multiplication;
+    }
+
+    public boolean isDivision() {
+        return division;
+    }
+
+    public void setDivision(boolean division) {
+        this.division = division;
     }
 }
